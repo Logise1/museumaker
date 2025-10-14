d85f2d1e244ae2610d8cf7b2ff1fd88bf0879fa0
Logise1 <logise1123@gmail.com>
2025-10-14 19:09:29 +0200

    Añado import de drawingColors

--- ui-handler.js
+++ ui-handler.js
@@ -1,7 +1,9 @@
 import { onValue, push, set, get, query, orderByChild, limitToFirst, serverTimestamp, ref } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-database.js";
-import { getSelectedObject, updateRoomDimensions as updateRoomDimensions3D, markAsDirty, textureUrls, textureNames, setCurrentMusic, drawingColors, setCurrentDrawingColor, getDb, getMuseumId, clearScene as clearScene3D, objects, roomGroups, getRenderer, drawingCanvases, placePainting } from './three-scene.js';
+import { getSelectedObject, updateRoomDimensions as updateRoomDimensions3D, markAsDirty, textureUrls, textureNames, setCurrentMusic, setCurrentDrawingColor, getDb, getMuseumId, clearScene as clearScene3D, objects, roomGroups, getRenderer, drawingCanvases, placePainting } from './three-scene.js';
 
 let callbacks = {};
+const drawingColors = ['#000000', '#ef4444', '#f97316', '#eab308', '#22c55e', '#3b82f6', '#8b5cf6', '#ffffff'];
+
 
 // --- VIEW MANAGEMENT ---
 const views = {
@@ -545,4 +547,5 @@ function updateMuteButtonUI() {
     const muteIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 16 16"><path d="M6.717 3.55A.5.5 0 0 1 7 4v8a.5.5 0 0 1-.812.39L3.825 10.5H1.5A.5.5 0 0 1 1 10V6a.5.5 0 0 1 .5-.5h2.325l2.363-1.89a.5.5 0 0 1 .529-.06zM6 5.04 4.312 6.39A.5.5 0 0 1 4 6.5H2v3h2a.5.5 0 0 1 .312.11L6 10.96V5.04zm7.854.606a.5.5 0 0 1 0 .708L12.207 8l1.647 1.646a.5.5 0 0 1-.708.708L11.5 8.707l-1.646 1.647a.5.5 0 0 1-.708-.708L10.793 8 9.146 6.354a.5.5 0 1 1 .708-.708L11.5 7.293l1.646-1.647a.5.5 0 0 1 .708 0z"/></svg>`;
     btn.innerHTML = audio.muted ? playIcon : muteIcon;
 }
+
 
